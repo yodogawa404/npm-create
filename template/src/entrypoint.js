eabae34897ef954c33e8275ff590e0eaef0f0310
yodogawa404 <shiontak2oo5@gmail.com>
2026-02-08 05:54:44 +0900

    Interの更新

--- template/src/entrypoint.js
+++ template/src/entrypoint.js
@@ -1,4 +1,4 @@
-import '@yodogawa404/font-inter/index.css';
+import '@yodogawa404/font-inter/alphabets.css';
 import '@yodogawa404/line-seed-jp/hiragana.css';
 import '@yodogawa404/line-seed-jp/katakana.css';
 import '@yodogawa404/noto-sans-jp/index.css';
